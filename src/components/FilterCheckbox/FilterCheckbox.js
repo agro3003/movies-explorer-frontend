1d52a806a11918cffee8d666168f2dd084df165a
Roman Kononov <doubleempty@yandex.ru>
2022-08-16 20:49:35 +0300

    change class

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -3,12 +3,12 @@ import './FilterCheckbox.css'
 export default function FilterCheckbox() {
 
   return (
-    <label className="switch">
-      <input className="switch__default" type="checkbox" />
+    <label className="filter-checkbox">
+      <input className="filter-checkbox__default" type="checkbox" />
       <span
-        className="switch__slider"
+        className="filter-checkbox__slider"
       />
-      <span className="switch__label">Короткометражки</span>
+      <span className="filter-checkbox__label">Короткометражки</span>
     </label>
   )
 }
